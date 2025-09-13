1c70cb2cf7c9b7cef6333e9aa4e2deae847e6448
Kartikey Rana <44106280+kartikey09@users.noreply.github.com>
2025-09-14 01:49:45 +0530

    Executor Service

--- LLD1/src/Main.java
+++ LLD1/src/Main.java
@@ -1,11 +1,19 @@
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        System.out.printf("\nhello %d\n", 5);
 
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for(int i = 0; i < 100; i++) {
+            if(i == 5 || i == 10 || i == 95)
+                    System.out.println("hello");
+            NumberPrinter nb = new NumberPrinter(i);
+            executorService.submit(nb);
+        }
+        executorService.shutdown();
     }
 }
---
+++ LLD1/src/NumberPrinter.java
@@ -0,0 +1,12 @@
+public class NumberPrinter implements Runnable{
+    private int number;
+
+    public NumberPrinter(int number){
+        this.number = number;
+    }
+
+    @Override
+    public void run(){
+        System.out.println("The number is : " + this.number + " and the thread is " + Thread.currentThread().getName());
+    }
+}
